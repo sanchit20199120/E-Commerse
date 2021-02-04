3a0b7d0761667c7d2d577ae024734745f8c92d5b
sanchit20199120 <sanchit.bhard@gmail.com>
2021-02-04 12:30:03 -0500

    modification

--- website/website/urls.py
+++ website/website/urls.py
@@ -19,5 +19,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls'))
+    path('accounts/', include('allauth.urls')),
+    path('', include('jeans.urls', namespace='jeans'))
 ]
